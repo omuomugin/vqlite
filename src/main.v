623aba35a6d65c701462651f1dba9cab1df055b7
omuomugin <omuomugin@out-box.dev>
2020-08-23 16:36:59 +0900

    feature: add in-memory db (#2)
    Part 3
    https://cstack.github.io/db_tutorial/parts/part3.html

--- src/main.v
+++ src/main.v
@@ -1,31 +1,48 @@
-import readline
+module main
 
-enum Meta_Command_Result {
+import readline
+import util
+
+const (
+	table_max_size = 100
+)
+
+enum MetaCommandResult {
 	success
 	exit
 }
 
 struct Statement {
-	statement_type Statement_type
+	statement_type StatementType
+	row_to_insert  Row
 }
 
-enum Statement_type {
+enum StatementType {
 	type_insert
 	type_select
+}
+
+struct Row {
+	id       u32
+	username string
+	email    string
+}
+
+struct Table {
+mut:
+	num_rows u32
+	pages    []Row
 }
 
 fn main() {
 	prompt := 'db > '
 	mut readline := readline.Readline{}
+	mut table := Table{}
 	for {
 		oline := readline.read_line(prompt) or {
-			println('[Error] cannot read line')
 			break
 		}
 		input := oline.trim_space()
-		if input == '.exit' {
-			break
-		}
 		meta_command_result := do_meta_command(input) or {
 			println(err)
 			continue
@@ -43,37 +60,70 @@ fn main() {
 			println(err)
 			continue
 		}
-		execute_statement(statement)
+		execute_statement(statement, mut table)
 	}
 }
 
-fn do_meta_command(command string) ?Meta_Command_Result {
+fn do_meta_command(command string) ?MetaCommandResult {
 	if command.starts_with('.') {
 		if command == '.exit' {
-			return Meta_Command_Result.exit
+			return MetaCommandResult.exit
 		} else {
-			return error('Unrecognized command $command')
+			return error("Unrecognized command \'$command\'")
 		}
 	}
-	return Meta_Command_Result.success
+	return MetaCommandResult.success
 }
 
 fn prepare_statement(command string) ?Statement {
-	if command.starts_with('insert') {
-		return Statement{
-			statement_type: Statement_type.type_insert
+	// [0]: command, [1]: arg ...
+	command_args := command.split(' ')
+	if command_args[0] == 'insert' {
+		// command should be `insert :id :username :email`
+		if command_args.len < 3 {
+			return error("Syntax error \'$command\'")
 		}
-	} else if command.starts_with('select') {
+		// if the first element was not `int` return error
+		id := util.s_to_u32(command_args[1]) or {
+			println(err)
+			return error("Syntax error \'$command\'")
+		}
+		row_to_insert := Row{
+			id: id
+			username: command_args[2]
+			email: command_args[3]
+		}
 		return Statement{
-			statement_type: Statement_type.type_select
+			statement_type: StatementType.type_insert
+			row_to_insert: row_to_insert
+		}
+	} else if command_args[0] == 'select' {
+		return Statement{
+			statement_type: StatementType.type_select
 		}
 	}
-	return error('Unrecognized statement $command')
+	return error("Unrecognized statement \'$command\'")
 }
 
-fn execute_statement(statement Statement) {
+fn execute_statement(statement Statement, mut table Table) {
 	match statement.statement_type {
-		.type_insert { println('This is where we would do an insert.') }
-		.type_select { println('This is where we would do a select.') }
+		.type_insert { execute_insert(statement, mut table) or {
+				println(err)
+			} }
+		.type_select { execute_select(statement, mut table) or {
+				println(err)
+			} }
 	}
+}
+
+fn execute_insert(statement Statement, mut table Table) ? {
+	if table.num_rows >= table_max_size {
+		return error('failed to insert table was full. max page size is $table_max_size')
+	}
+	table.pages << statement.row_to_insert
+	table.num_rows = table.num_rows + 1
+}
+
+fn execute_select(statement Statement, mut table Table) ? {
+	println('$table.pages')
 }
