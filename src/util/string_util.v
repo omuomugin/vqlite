52842cde03da3dc8f951dbc6a9d6b32b3e5d17b6
omuomugin <omuomugin@out-box.dev>
2020-08-24 09:29:13 +0900

    refactor: add comment on why to check 0 when parsing

--- src/util/string_util.v
+++ src/util/string_util.v
@@ -5,6 +5,7 @@ pub fn s_to_u32(str string) ?u32 {
 		return 0
 	}
 	parsed_int := str.u32()
+	// when failed parse, `str.u32()` will return 0
 	if parsed_int == 0 {
 		return error("string parsing to u32 failed \'$str\'")
 	}
